c0403d62eb43d85a59e6960d3480687129c7df8f
sjomansa <sjomansaku@gmail.com>
2024-12-01 10:17:28 +0200

    Add ON DELETE and REFERENCE for schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,60 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN);
-CREATE TABLE restaurants (id SERIAL PRIMARY KEY, owner_id INTEGER REFERENCES users, name TEXT UNIQUE, address TEXT, city TEXT);
-CREATE TABLE r_stars (id SERIAL PRIMARY KEY, r_id INTEGER REFERENCES restaurants, rating INTEGER);
-CREATE TABLE r_quetimes (id SERIAL PRIMARY KEY, r_id INTEGER REFERENCES restaurants, que_time INTEGER);
-CREATE TABLE r_specialities (id SERIAL PRIMARY KEY, r_id INTEGER REFERENCES restaurants, speciality TEXT);
-CREATE TABLE r_menus (id SERIAL PRIMARY KEY, r_id INTEGER REFERENCES restaurants, food TEXT, price NUMERIC);
-CREATE TABLE r_info (id SERIAL PRIMARY KEY, r_id INTEGER REFERENCES restaurants, infotext TEXT, open_times TEXT);
-CREATE TABLE likes_specialities (id SERIAL PRIMARY KEY, liker_id INTEGER REFERENCES users, speciality_id INTEGER REFERENCES r_specialities);
-CREATE TABLE messages (id SERIAL PRIMARY KEY, r_id INTEGER REFERENCES restaurants, u_id INTEGER REFERENCES users, message TEXT, time TIMESTAMP);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE NOT NULL, 
+    password TEXT NOT NULL, 
+    admin BOOLEAN
+);
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY, 
+    owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+    name TEXT UNIQUE NOT NULL, 
+    address TEXT NOT NULL, 
+    city TEXT NOT NULL
+);
+
+CREATE TABLE r_stars (
+    id SERIAL PRIMARY KEY, 
+    r_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    rating INTEGER
+);
+
+CREATE TABLE r_quetimes (
+    id SERIAL PRIMARY KEY, 
+    r_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    que_time INTEGER
+);
+
+CREATE TABLE r_specialities (
+    id SERIAL PRIMARY KEY, 
+    r_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    speciality TEXT
+);
+
+CREATE TABLE r_menus (
+    id SERIAL PRIMARY KEY, 
+    r_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    food TEXT NOT NULL, 
+    price NUMERIC NOT NULL
+);
+
+CREATE TABLE r_info (
+    id SERIAL PRIMARY KEY, 
+    r_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    infotext TEXT, 
+    open_times TEXT
+);
+
+CREATE TABLE likes_specialities (
+    id SERIAL PRIMARY KEY, 
+    liker_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+    speciality_id INTEGER REFERENCES r_specialities(id) ON DELETE CASCADE
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY, 
+    r_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    u_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+    message TEXT, 
+    time TIMESTAMP
+);
