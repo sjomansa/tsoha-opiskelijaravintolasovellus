c6a06eab760051b08c5c1ef71c619afd26d5122d
sjomansa <sjomansaku@gmail.com>
2024-12-04 13:59:39 +0200

    add functionality for adding quetime

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@
 from app import app
 from flask import redirect, render_template, request, session
 from users import login_user, logout_user, register_user
-from restaurantdata import get_main_restaurantdata, get_singular_restaurantdata, create_new_restaurant, update_menu, update_restaurant_info, insert_menuitem, delete_menuitem, insert_rating
+from restaurantdata import get_main_restaurantdata, get_singular_restaurantdata, create_new_restaurant, update_menu, update_restaurant_info, insert_menuitem, delete_menuitem, insert_rating, insert_quetime
 from comments import insert_comment
 
 @app.route("/")
@@ -96,39 +96,28 @@ def restaurants_view():
     return render_template("restaurants.html", restaurants=restaurants_data)
 
 
-@app.route("/restaurants/<name>")
+@app.route("/restaurants/<name>", methods=["GET", "POST"])
 def restaurant_info(name):
-
-    # sql = text("""
-    #         SELECT R.id AS id, R.name AS name, U.username AS owner, COALESCE(ROUND(AVG(r_quetimes.que_time), 1),1) AS wait_time, COALESCE(ROUND(AVG(r_stars.rating), 1), 0) AS rating,
-    #         R.city AS city, R.address AS address FROM restaurants R JOIN users U ON U.id = R.owner_id LEFT JOIN r_stars ON r_stars.r_id = R.id LEFT JOIN r_quetimes ON r_quetimes.r_id = R.id
-    #         WHERE R.name =:name GROUP BY R.id, R.name, U.username, R.city, R.address;
-    #         """)
-    # result = db.session.execute(sql, {"name":name})
-    # restaurant = result.fetchone()
-
-    # sql = text("""
-    #         SELECT r_i.infotext AS infotext, r_i.open_times AS open_times FROM restaurants LEFT JOIN r_info r_i ON r_i.r_id = restaurants.id 
-    #         WHERE restaurants.name =:name;
-    #         """)
-    # result = db.session.execute(sql, {"name":name})
-    # info = result.fetchone()
-
-    # sql = text("""
-    #     SELECT M.food AS food, M.price AS price FROM restaurants R LEFT JOIN r_menus AS M ON R.id = M.r_id 
-    #     WHERE R.name =:name;
-    #     """)
-    # result = db.session.execute(sql, {"name":name})
-    # menu = result.fetchall()
-
-    # messages = get_messages(restaurant.id)
 
     restaurant, info, menu, messages = get_singular_restaurantdata(name)
 
     if restaurant is None:
-        return render_template("error.html", message = "No restaurant found, are you sure this restaurant exists?")
+            return render_template("error.html", message = "No restaurant found, are you sure this restaurant exists?")
 
-    return render_template("restaurantlayout.html", restaurant=restaurant, info=info, menu=menu, messages=messages)
+    if request.method == "GET":
+
+        return render_template("restaurantlayout.html", restaurant=restaurant, info=info, menu=menu, messages=messages)
+    
+    else:
+
+        try:
+            r_id = restaurant.id
+            quetime = int(request.form["quetime"])
+            insert_quetime(r_id, quetime)
+            return redirect(f"/restaurants/{name}")
+        
+        except:  
+            return render_template("error.html", message="Ongelma odotusajan lisäämisessä palvelimelle")
 
 
 
